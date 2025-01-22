0a40bdda13ffc2d4d4a2d8ae4d5716eb84837d98
name <abc@def.com>
2025-01-23 00:53:19 +0900

    dev + feat/6

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
       },
       {
         path: 'login',
-        // element: <LoginPage/>
+        element: <LoginPage/>
       },
       {
         path: 'register',
