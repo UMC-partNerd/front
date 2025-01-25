cde3fb544ad095c885a3ac52bec8265f68d0592b
seo yun <yuxxni05@gmail.com>
2025-01-25 21:00:35 +0900

    .

--- src/layout/root-layout.jsx
+++ src/layout/root-layout.jsx
@@ -1,18 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Navbar from '../components/common/Navbar';
+import Footer from '../components/common/Footer';
 import { useState, useEffect } from 'react';
 
 function RootLayout() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // 기본적으로 로그인되지 않은 상태로 설정
+  const [isLoggedIn, setIsLoggedIn] = useState(false);  
   const [isFooterVisible, setIsFooterVisible] = useState(false);
 
   const handleScroll = () => {
     const scrollPosition = window.scrollY + window.innerHeight;
     const documentHeight = document.documentElement.scrollHeight;
 
-    setIsFooterVisible(scrollPosition >= documentHeight);
+
+    setIsFooterVisible(scrollPosition >= documentHeight + 100);
   };
 
   useEffect(() => {
