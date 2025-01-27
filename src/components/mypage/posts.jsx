aa5151d80a0bee2bca61ce9ba52795cd19e2c0c7
chy031202 <chy031202@gmail.com>
2025-01-27 16:41:35 +0900

    내가 쓴 글

--- src/components/mypage/posts.jsx
+++ src/components/mypage/posts.jsx
@@ -1,10 +1,15 @@
 import styled from "styled-components"
+import { MainWrapp, Title } from "../../styles/mypagestyles";
+import PostFeed from "./mypost_feed";
 
 const MyPostsComp = () =>{
     return(
-        <>
-        <h3>내가 쓴 글</h3>
-        </>
+        <MainWrapp>
+        <Title>내가 쓴 글</Title>
+
+        <h3>카테고리 넣을 자리</h3>
+        <PostFeed/>
+        </MainWrapp>
     )
 }
 
