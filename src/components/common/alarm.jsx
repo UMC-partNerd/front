1b1dcb7609325216b2161974ce6738b163eb85fe
name <abc@def.com>
2025-01-31 03:40:36 +0900

    알림 홈 : 목록 UI

--- src/components/common/alarm.jsx
+++ src/components/common/alarm.jsx
@@ -3,9 +3,12 @@ import { useRecoilValue } from 'recoil';
 import { alarmCountState } from '../../hooks/atom';
 
 import {
+    Container,
     AlarmWrap1,
+    Title,
     AlarmWrap2,
     AlarmContainer,
+    Row,
     Indicator,
     AlarmMessage,
     Timer,
@@ -19,27 +22,30 @@ function Alarm({ club, timer }) {
     const chevronRight = "chevron-right.png";
 
     return (
-        <>
+        <Container>
             <AlarmWrap1>
-            알림
+                <Title>알림</Title>
                 <AlarmWrap2>
 
                     <AlarmContainer>
-                        <Indicator>
-                            <img src={`/${Ellipse427}`} alt="안 읽은 알림" />
-                        </Indicator>
+                        <Row>
+                            <Indicator>
+                                <img src={`/${Ellipse427}`} alt="안 읽은 알림" />
+                            </Indicator>
 
-                        <AlarmMessage msg="club">{club}</AlarmMessage>
-                        <AlarmMessage>로부터 콜라보 신청이 왔습니다.</AlarmMessage>
-                        <Timer>{timer}</Timer>
-                        
-                        <From>확인하러 가기<img src={`/${chevronRight}`} alt="확인하러 가기" /></From>
+                            <AlarmMessage msg="club">{club}</AlarmMessage>
+                            <AlarmMessage>로부터 콜라보 신청이 왔습니다.</AlarmMessage>
+                        </Row>
+                        <Row>
+                            <From>확인하러 가기<img src={`/${chevronRight}`} alt="확인하러 가기" /></From>
+                            <Timer>{timer}방금전</Timer>
+                        </Row>
                         <StyledPath><path d="M0 1L452 1.00004" /></StyledPath>
                     </AlarmContainer>
 
                 </AlarmWrap2>
             </AlarmWrap1>
-        </>
+        </Container>
     )
 }
 
--- src/styled-components/styled-Alarm.jsx
+++ src/styled-components/styled-Alarm.jsx
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  position: relative;
+  height: 100vh;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+  padding: 20px;
+`;
+
 export const AlarmWrap1 = styled.div`
   width: 547px;
   height: 747px;
@@ -7,6 +17,29 @@ export const AlarmWrap1 = styled.div`
   border-radius: 16px;
   background: #F3F4F7;
   box-shadow: 0px 2px 15px 0px rgba(0, 0, 0, 0.15);
+
+  position: absolute;
+  top: 20px;
+  right: 20px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px;
+
+  
+`;
+
+export const Title = styled.h3`
+  color: #212121;
+  font-family: Pretendard;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -0.64px;
+
+  margin-bottom: 20px;
 `;
 
 export const AlarmWrap2 = styled.div`
@@ -15,11 +48,27 @@ export const AlarmWrap2 = styled.div`
   flex-shrink: 0;
   border-radius: 8px;
   background: #FFFFFF;
-`;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px;`;
 
 export const AlarmContainer = styled.div`
-  width: 122px;
-  height: 452px;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  margin-bottom: 10px;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;  
+  width: 100%;
 `;
 
 export const Indicator = styled.circle`
@@ -27,6 +76,9 @@ export const Indicator = styled.circle`
   height: 12px;
   flex-shrink: 0;
   fill: var(--Main-Color, #0D29B7);
+
+  margin-bottom: 8px;
+  margin-right: 20px;
 `;
 
 export const AlarmMessage = styled.p`
@@ -37,6 +89,8 @@ export const AlarmMessage = styled.p`
   font-weight: ${props => props.msg === 'club' ? 600 : 500};
   line-height: normal;
   letter-spacing: -0.4px;
+
+  margin-bottom: 5px;
 `;
 
 export const Timer = styled.p`
@@ -48,6 +102,8 @@ export const Timer = styled.p`
   font-weight: 500;
   line-height: normal;
   letter-spacing: -0.28px;
+
+  margin-bottom: 10px;
 `;
 
 export const From = styled.p`
@@ -56,15 +112,18 @@ export const From = styled.p`
   font-size: 18px;
   font-style: normal;
   font-weight: 500;
-  line-height: 22px; /* 122.222% */
+  line-height: 22px;
   letter-spacing: -0.36px;
+
+｣  margin-left: auto;
+  margin-top: 10px;
 `;
 
-const StyledPath = styled.svg.attrs({
+export const StyledPath = styled.svg.attrs({
   xmlns: "http://www.w3.org/2000/svg",
   viewBox: "0 0 452 2"
 })`
-  width: 452px;
+  width: 100%;
   height: 2px;
   fill: none;
 
