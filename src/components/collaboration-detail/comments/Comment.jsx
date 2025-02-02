a173d6eefd1b2ea453750864ad058707521044dc
seo yun <yuxxni05@gmail.com>
2025-02-02 13:01:00 +0900

    경로수정

--- src/components/collaboration-detail/comments/Comment.jsx
+++ src/components/collaboration-detail/comments/Comment.jsx
@@ -3,7 +3,7 @@ import { FiMoreVertical } from "react-icons/fi";
 import { PiArrowElbowDownRightBold } from "react-icons/pi";
 import Reply from './Reply';
 import ReplyInput from './ReplyInput';
-import * as S from '../../styled-components/collab-styles/styled-Comment'; 
+import * as S from '../../../styled-components/collab-styles/styled-Comment'; 
 
 const formatDate = (date) => {
   const d = new Date(date);
